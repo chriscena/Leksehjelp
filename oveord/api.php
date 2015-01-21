8a8739522a088f073aded36d24498796fb49902b
chriscena <chris+github@cenaonline.net>
2015-01-21 22:39:25 +0100

    Initial commit
    This is the first working version of Øveord in Leksehjelp.

---
+++ oveord/api.php
@@ -0,0 +1,47 @@
+<?php
+require_once('admin.inc');
+header('Content-Type: application/json');
+
+$result = "[]";
+
+if (count($_POST) > 0) {
+	$action = filter_input(INPUT_POST, 'a', FILTER_SANITIZE_STRING);
+
+	switch ($action) {
+		case 'createword':
+			$collectionId = filter_input(INPUT_POST, 'cid', FILTER_SANITIZE_NUMBER_INT);
+			$norwegianWord = filter_input(INPUT_POST, 'nw', FILTER_SANITIZE_STRING);
+			$norwegianWordAudio = CreateAudioFile('no', $norwegianWord);
+			
+			$lang = filter_input(INPUT_POST, 'l', FILTER_SANITIZE_STRING);
+			$foreignWordAudio = null;
+			$foreignWord = null;
+			if ($lang != "no") {
+				$foreignWord = filter_input(INPUT_POST, 'fw', FILTER_SANITIZE_STRING);
+				$foreignWordAudio = CreateAudioFile($lang, $foreignWord);
+			}
+
+			$result = CreateWord($collectionId, $lang, $norwegianWord, $norwegianWordAudio, $foreignWord, $foreignWordAudio);
+			break;
+		default:
+			$result = "[]";
+			break;
+	}
+} else {
+	$action = filter_input(INPUT_GET, 'a', FILTER_SANITIZE_STRING);
+
+	switch ($action) {
+		case 'getcollections':
+			$result = GetCollections();
+			break;
+		case 'getwords':
+			$index = filter_input(INPUT_GET, 'i', FILTER_SANITIZE_NUMBER_INT);
+			$result = GetWords($index);
+			break;
+		default:
+			$result = "[]";
+			break;
+	}
+}
+
+echo json_encode($result);
