647238738deebbbf174add65f23b08e41a86f9f3
chriscena <chris+github@cenaonline.net>
2015-01-24 00:36:48 +0100

    Collection now only supports one language.
    Language now part of collection. Randomized words. Create Collection
    method.

--- oveord/api.php
+++ oveord/api.php
@@ -1,44 +1,49 @@
 <?php
-require_once('admin.inc');
-header('Content-Type: application/json');
+require_once("admin.inc");
+header("Content-Type: application/json");
 
 $result = "[]";
 
-if (count($_POST) > 0) {
-	$action = filter_input(INPUT_POST, 'a', FILTER_SANITIZE_STRING);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$action = filter_input(INPUT_POST, "a", FILTER_SANITIZE_STRING);
 
 	switch ($action) {
-		case 'cw':
-			$collectionId = filter_input(INPUT_POST, 'cid', FILTER_SANITIZE_NUMBER_INT);
-			$norwegianWord = filter_input(INPUT_POST, 'nw', FILTER_SANITIZE_STRING);
-			$norwegianWordAudio = CreateAudioFile('no', $norwegianWord);
+		case "cc":
+			$name = filter_input(INPUT_POST, "n", FILTER_SANITIZE_STRING);
+			$language = filter_input(INPUT_POST, "l", FILTER_SANITIZE_STRING);
+			$result = CreateCollection($name, $language);
+			break;
+		case "cw":
+			$collectionId = filter_input(INPUT_POST, "cid", FILTER_SANITIZE_NUMBER_INT);
+			$norwegianWord = filter_input(INPUT_POST, "nw", FILTER_SANITIZE_STRING);
+			$norwegianWordAudio = CreateAudioFile("no", $norwegianWord);
 			
-			$lang = filter_input(INPUT_POST, 'l', FILTER_SANITIZE_STRING);
+			$lang = filter_input(INPUT_POST, "l", FILTER_SANITIZE_STRING);
 			$foreignWordAudio = null;
 			$foreignWord = null;
 			if ($lang != "no") {
-				$foreignWord = filter_input(INPUT_POST, 'fw', FILTER_SANITIZE_STRING);
+				$foreignWord = filter_input(INPUT_POST, "fw", FILTER_SANITIZE_STRING);
 				$foreignWordAudio = CreateAudioFile($lang, $foreignWord);
 			}
 
 			$result = CreateWord($collectionId, $lang, $norwegianWord, $norwegianWordAudio, $foreignWord, $foreignWordAudio);
 			break;
-		case 'dw':
-			$index = filter_input(INPUT_GET, 'i', FILTER_SANITIZE_NUMBER_INT);
+		case "dw":
+			$index = filter_input(INPUT_POST, "i", FILTER_SANITIZE_NUMBER_INT);
 			DeleteWord($index);
 		default:
 			$result = "[]";
 			break;
 	}
-} else {
-	$action = filter_input(INPUT_GET, 'a', FILTER_SANITIZE_STRING);
+} elseif ($_SERVER["REQUEST_METHOD"] == "GET") {
+	$action = filter_input(INPUT_GET, "a", FILTER_SANITIZE_STRING);
 
 	switch ($action) {
-		case 'gc':
+		case "gc":
 			$result = GetCollections();
 			break;
-		case 'gw':
-			$index = filter_input(INPUT_GET, 'i', FILTER_SANITIZE_NUMBER_INT);
+		case "gw":
+			$index = filter_input(INPUT_GET, "i", FILTER_SANITIZE_NUMBER_INT);
 			$result = GetWords($index);
 			break;
 		default:
